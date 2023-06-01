99f8b26c168972428433f4e5f1b28d9764da59f8
Guglielmo Galasso <guglielmo.galasso1@gmail.com>
2023-06-01 11:35:19 +0100

    done

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const Mood = require('../models/Mood.model')
 
-router.get("/", (req, res, next) => {
-  res.json("All good in here");
+
+router.post("/create-mood", async (req, res, next) => {
+const {user, mood, comment } = req.body;
+try {
+  let response = await Mood.create({user, mood, comment, moodSound: []})
+  res.json(response);
+
+} catch (error) {
+  res.json(error);
+}
 });
 
 module.exports = router;
